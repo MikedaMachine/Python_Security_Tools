fb1187fc85d665b4e697116749b330551db0f939
Kali <msimmo94@students.kennesaw.edu>
2021-09-23 10:40:46 -0400

    Allow port scanning of multiple targets

--- PortScanner.py
+++ PortScanner.py
@@ -1,12 +1,39 @@
 import socket
 from IPy import IP
 
-ip_address = input('Enter target to scan:')
-port = 80
+def scan(ip_address):
+    changed_ip = inspect_ip(ip_address)
+    print('\n' + 'Scanning Target ip_address ' + str(ip_address))
+    for port in range(75,85):
+        scanner(changed_ip, port)
 
-try:
-    sock = socket.socket()
-    sock.connect((ip_address, port))
-    print('Port 80 is open')
-except:
-    print('Port 80 is closed')
+def inspect_ip(ip):
+    try:
+        IP(ip)
+        return(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+def grab_banner(s):
+    return s.recv(1024)
+
+def scanner(ip_address, port):
+    try:
+        runner = socket.socket()
+        runner.settimeout(0.5)
+        runner.connect((ip_address, port))
+        try:
+            banner = grab_banner(runner)
+            print('Open Port ' + str(port) + ' : ' + str(banner.decode().strip('\n')))
+        except:
+            print('Open Port ' + str(port)) 
+    except:
+        pass
+
+
+ip_address = input('Enter targets to scan(seperate targets with a comma(,) ): ')
+if ',' in ip_address:
+    for ip_add in ip_address.split(','):
+        scan(ip_add.strip(' '))
+else:
+    scan(ip_address)
